6532bb998629afe9435dad7b0a553c9f8863bc60
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-13 11:11:13 +0300

    Implement using an enum class

--- src/main/kotlin/ResistorColor.kt
+++ src/main/kotlin/ResistorColor.kt
@@ -1,11 +1,19 @@
+private enum class ColorCodes {
+    black,
+    brown,
+    red,
+    orange,
+    yellow,
+    green,
+    blue,
+    violet,
+    grey,
+    white
+}
+
 object ResistorColor {
 
-    fun colorCode(input: String): Int {
-        TODO("Implement this to complete the task")
-    }
+    fun colorCode(input: String) = ColorCodes.valueOf(input).ordinal
 
-    fun colors(): List<String> {
-        TODO("Implement this to complete the task")
-    }
-
+    fun colors(): List<String> = ColorCodes.values().map { a -> a.name }
 }
